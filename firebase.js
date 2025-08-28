889c5320d3857eca081f25bc64d65d60ee769ac6
ChXz-pkl <chandrxz76@gmail.com>
2025-08-28 11:04:27 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,10 @@
 // 2. FIREBASE CONFIG & INIT
 const firebaseConfig = {
-    apiKey: "AIzaSyAd-Ln8l9WyOqaGEwipvXSFkimjFR6RjI8",
-    authDomain: "journify-chx.firebaseapp.com",
-    projectId: "journify-chx",
-    storageBucket: "journify-chx.firebasestorage.app",
-    messagingSenderId: "527828409650",
-    appId: "1:527828409650:web:1a1e659850f4c4a412cb2b",
-    measurementId: "G-9EB83J1XN7"
-  };
+  apiKey: "AIzaSyAzx4v-4I9NYrdOLeraZ8u1HgLwa7rFlBA",
+  authDomain: "life-quest-system-chx.firebaseapp.com",
+  projectId: "life-quest-system-chx",
+  storageBucket: "life-quest-system-chx.firebasestorage.app",
+  messagingSenderId: "986951587369",
+  appId: "1:986951587369:web:e5340c92964e4ca3986e38",
+  measurementId: "G-JTWKTRD007"
+};
